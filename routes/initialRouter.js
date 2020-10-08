67c1d3889c666d9c6015bbca8daca41b2136907d
unknown <kulesza.michal@gmail.com>
2020-10-08 14:48:29 +0200

    add: route for series and films

--- routes/initialRouter.js
+++ routes/initialRouter.js
@@ -50,7 +50,7 @@ router.get("/browse", async (req, res) => {
 	}
 });
 
-//Initial browse data
+//Initial series data
 router.get("/series", async (req, res) => {
 	console.log("REQUEST /SERIES");
 	try {
@@ -58,12 +58,68 @@ router.get("/series", async (req, res) => {
 		const tvGenres = axios.get(`https://api.themoviedb.org/3/genre/tv/list?api_key=${process.env.API_KEY}`);
 		const baseUrl = axios.get(`https://api.themoviedb.org/3/configuration?api_key=${process.env.API_KEY}`);
 
-		const popular = await axios.get(`https://api.themoviedb.org/3/tv/popular?api_key=${process.env.API_KEY}`);
-		const top_rated = await axios.get(`https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.API_KEY}`);
-		const on_the_air = await axios.get(`https://api.themoviedb.org/3/tv/on_the_air?api_key=${process.env.API_KEY}`);
+		const popular = axios.get(`https://api.themoviedb.org/3/tv/popular?api_key=${process.env.API_KEY}`);
+		const top_rated = axios.get(`https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.API_KEY}`);
+		const on_the_air = axios.get(`https://api.themoviedb.org/3/tv/on_the_air?api_key=${process.env.API_KEY}`);
+		const discover = axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.API_KEY}`);
 
 		axios
-			.all([movieGenres, tvGenres, baseUrl, popular, top_rated, on_the_air])
+			.all([movieGenres, tvGenres, baseUrl, popular, top_rated, on_the_air, discover])
+			.then(
+				axios.spread((...responses) => {
+					const movieGenres = responses[0];
+					const tvGenres = responses[1];
+					const genres = [...movieGenres.data.genres, ...tvGenres.data.genres];
+					const baseUrl = responses[2].data.images.base_url;
+
+					const popular = appendMediaType(
+						appendBaseUrl(decodeGenres(responses[3].data.results, genres), baseUrl),
+						"tv"
+					);
+
+					const top_rated = appendMediaType(
+						appendBaseUrl(decodeGenres(responses[4].data.results, genres), baseUrl),
+						"tv"
+					);
+
+					const on_the_air = appendMediaType(
+						appendBaseUrl(decodeGenres(responses[5].data.results, genres), baseUrl),
+						"tv"
+					);
+
+					const discover = appendMediaType(
+						appendBaseUrl(decodeGenres(responses[6].data.results, genres), baseUrl),
+						"tv"
+					);
+
+					res.send([{ popular }, { top_rated }, { on_the_air }, { discover }]);
+				})
+			)
+			.catch(err => {
+				res.status(500).json({ error: err.message });
+				console.log(err.message);
+			});
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+		err => console.log(err.message);
+	}
+});
+
+//Initial films data
+router.get("/films", async (req, res) => {
+	console.log("REQUEST /FILMS");
+	try {
+		const movieGenres = axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.API_KEY}`);
+		const tvGenres = axios.get(`https://api.themoviedb.org/3/genre/tv/list?api_key=${process.env.API_KEY}`);
+		const baseUrl = axios.get(`https://api.themoviedb.org/3/configuration?api_key=${process.env.API_KEY}`);
+
+		const popular = axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`);
+		const top_rated = axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}`);
+		const trending = axios.get(`https://api.themoviedb.org/3/trending/films/week?api_key=${process.env.API_KEY}`);
+		const discover = axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}`);
+
+		axios
+			.all([movieGenres, tvGenres, baseUrl, popular, top_rated, trending, discover])
 			.then(
 				axios.spread((...responses) => {
 					const movieGenres = responses[0];
@@ -81,17 +137,25 @@ router.get("/series", async (req, res) => {
 						"movie"
 					);
 
-					const on_the_air = appendMediaType(
+					const trending = appendMediaType(
 						appendBaseUrl(decodeGenres(responses[5].data.results, genres), baseUrl),
 						"movie"
 					);
 
-					res.send([{ popular }, { top_rated }, { on_the_air }]);
+					const discover = appendMediaType(
+						appendBaseUrl(decodeGenres(responses[6].data.results, genres), baseUrl),
+						"movie"
+					);
+					res.send([{ popular }, { top_rated }, { trending }, { discover }]);
 				})
 			)
-			.catch(err => console.log(err.message));
+			.catch(err => {
+				res.status(500).json({ error: err.message });
+				console.log(err.message);
+			});
 	} catch (err) {
 		res.status(500).json({ error: err.message });
+		err => console.log(err.message);
 	}
 });
 
