f086c11f57631483f209400c53f47f4521a902f4
Michal <kulesza.michal@gmail.com>
2020-10-20 15:02:09 +0200

    fix: append media type and decode genres

--- helpers/decodeGenres.js
+++ helpers/decodeGenres.js
@@ -1,6 +1,9 @@
 const decodeGenres = (arr, genres) => {
 	arr.forEach(
-		video => (video.genre_ids = video.genre_ids.map(id => genres.find(genre => genre.id === id)).map(res => res.name))
+		video =>
+			(video.genre_ids = video.genre_ids
+				.map(id => genres.find(genre => genre.id === id))
+				.map(res => (res ? res.name : "")))
 	);
 	return arr;
 };
--- routes/genreRouter.js
+++ routes/genreRouter.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const appendBaseUrl = require("../helpers/appendBaseUrl");
+const decodeGenres = require("../helpers/decodeGenres");
+const appendMediaType = require("../helpers/appendMediaType");
 
 const baseUrl = axios.get(`https://api.themoviedb.org/3/configuration?api_key=${process.env.API_KEY}`);
 
@@ -10,19 +12,21 @@ router.post("/series", async (req, res) => {
 	const { genreID, page } = req.body;
 	console.log("REQUEST /genre/series", genreID);
 	try {
-		const genres = axios.get(
-			`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.API_KEY}&with_genres=${genreID}&page=${page}`
+		const genresData = axios.get(
+			`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.API_KEY}&with_genresData=${genreID}&page=${page}`
 		);
+		const genresMap = axios.get(`https://api.themoviedb.org/3/genre/tv/list?api_key=${process.env.API_KEY}`);
 
 		axios
-			.all([baseUrl, genres])
+			.all([baseUrl, genresData, genresMap])
 			.then(
 				axios.spread((...responses) => {
 					const url = responses[0].data.images.base_url;
-					const genres = responses[1];
-					const data = appendBaseUrl(responses[1].data.results, url);
+					const genresData = responses[1];
+					const genresMap = responses[2].data.genres;
+					const data = appendMediaType(decodeGenres(appendBaseUrl(responses[1].data.results, url), genresMap), "tv");
 
-					res.send({ data: data, page: genres.data.page, total_pages: genres.data.total_pages });
+					res.send({ data: data, page: genresData.data.page, total_pages: genresData.data.total_pages });
 				})
 			)
 			.catch(err => {
@@ -40,19 +44,21 @@ router.post("/films", async (req, res) => {
 	const { genreID } = req.body;
 	console.log("REQUEST /genre/films", genreID);
 	try {
-		const genres = axios.get(
+		const genresData = axios.get(
 			`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&with_genres=${genreID}`
 		);
+		const genresMap = axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.API_KEY}`);
 
 		axios
-			.all([baseUrl, genres])
+			.all([baseUrl, genresData, genresMap])
 			.then(
 				axios.spread((...responses) => {
 					const url = responses[0].data.images.base_url;
-					const genres = responses[1];
-					const data = appendBaseUrl(responses[1].data.results, url);
+					const genresData = responses[1];
+					const genresMap = responses[2].data.genres;
+					const data = appendMediaType(decodeGenres(appendBaseUrl(responses[1].data.results, url), genresMap), "movie");
 
-					res.send({ data: data, page: genres.data.page, total_pages: genres.data.total_pages });
+					res.send({ data: data, page: genresData.data.page, total_pages: genresData.data.total_pages });
 				})
 			)
 			.catch(err => {
