3133fe861be2fa6e00e9af87d1962bcc0fef1102
Michal <kulesza.michal@gmail.com>
2020-10-28 09:57:01 +0100

    add: firebase admin

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-var cors = require("cors");
-const app = express();
+const cors = require("cors");
+const admin = require("firebase-admin");
 
 const initialRouter = require("./routes/initialRouter");
 const detailsRouter = require("./routes/detailsRouter");
@@ -10,9 +10,18 @@ const videoRouter = require("./routes/videoRouter");
 const genresRouter = require("./routes/genresRouter");
 const genreRouter = require("./routes/genreRouter");
 
-app.use(cors());
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://netflix-d2613.firebaseio.com",
+});
+
+const PORT = process.env.PORT || 8888;
+const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/initial", initialRouter);
 app.use("/details", detailsRouter);
@@ -21,4 +30,4 @@ app.use("/video", videoRouter);
 app.use("/genres", genresRouter);
 app.use("/genre", genreRouter);
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
